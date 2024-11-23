ba32bdca43852f13d4805bbad7509e535a6af0b8
Bartosz Podemski <bartekpodemski@gmail.com>
2024-11-23 20:02:12 +0100

    Generating access token and refresh token

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDTO } from 'src/auth/dto/registerUserDTO';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 
 @Controller('/api/auth')
 export class AuthController {
@@ -12,14 +19,37 @@ export class AuthController {
     @Body() registerUserDTO: RegisterUserDTO,
     @Res({ passthrough: true }) res: Response,
   ) {
-    const {user, token} = await this.authService.register(registerUserDTO);
+    const { user, at, rt } = await this.authService.register(registerUserDTO);
 
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 60 * 60 * 1000,
-    })
+    res.cookie('refreshToken', rt, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
 
-    return user;
+    return { user, at };
+  }
+
+  @Post('/refresh')
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const rt = req.cookies['refreshToken'];
+
+    if (!rt) {
+      throw new UnauthorizedException('Refresh token is required');
+    }
+
+    const { accessToken, refreshToken } =
+      await this.authService.refreshToken(rt);
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return { accessToken };
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { RegisterUserDTO } from 'src/auth/dto/registerUserDTO';
 import * as bcrypt from 'bcrypt';
@@ -35,9 +39,50 @@ export class AuthService {
       },
     });
 
-    const payload = { id: newUser.id, email: newUser.email };
-    const token = this.jwtService.sign(payload);
+    const { accessToken, refreshToken } = await this.generateTokens(newUser.id);
 
-    return { user: { name: newUser.name, email: newUser.email }, token };
+    return {
+      user: { name: newUser.name, email: newUser.email },
+      at: accessToken,
+      rt: refreshToken,
+    };
+  }
+
+  async generateTokens(userId: string) {
+    const payload = { id: userId };
+
+    const accessToken = this.jwtService.sign(payload, {
+      expiresIn: '15m',
+      secret: process.env.JWT_SECRET,
+    });
+
+    const refreshToken = this.jwtService.sign(payload, {
+      expiresIn: '7d',
+      secret: process.env.JWT_REFRESH_SECRET,
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  async refreshToken(refreshToken: string) {
+    try {
+      const payload = this.jwtService.verify(refreshToken, {
+        secret: process.env.JWT_REFRESH_SECRET,
+      });
+
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id: payload.id,
+        },
+      });
+
+      if (!user) {
+        throw new UnauthorizedException('Invalid token');
+      }
+
+      return this.generateTokens(user.id);
+    } catch (error) {
+      throw new UnauthorizedException('Invalid or expired token');
+    }
   }
 }
