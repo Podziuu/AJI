451a6217cf182b244e659abb3c420b88a68a4243
Bartosz Podemski <bartekpodemski@gmail.com>
2025-01-07 16:22:15 +0100

    Come back from checkout form to cart

--- frontend/src/pages/Checkout.tsx
+++ frontend/src/pages/Checkout.tsx
@@ -7,12 +7,17 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { MoveLeft } from "lucide-react";
 
 const Checkout = () => {
   const { cart } = useStore();
 
   return (
     <div>
+      <MoveLeft
+        className="ml-4 mt-4 cursor-pointer"
+        onClick={() => window.history.back()}
+      />
       <Card className="max-w-[450px] mx-auto mt-24 w-full text-center">
         <CardHeader>
           <CardTitle>Contact info</CardTitle>
